ae994d3116d962f293c1bb545ca48b097a0e9b38
FAMEC <biancacastro.bc90@gmail.com>
2024-09-05 10:50:14 -0300

    varias coisas

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo de Layout</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header>
+        <img id="logo" src="assets/img/LogoFAMEC.png" alt="logo">
+    </header>
+
+    <div id="q1" class="quadrado">
+        <img id="logo2" src="assets/img/logo.png" alt="">
+        <div>
+        <h3 class="text">Sabe aquela ajuda que faltava para
+        você escolher qual carreira seguir?</h3>
+        <p class="text">Faça o teste vocacional da FAMEC e descubra o seu maior talento.</p></div>
+    </div>
+
+    <div class="conteudo">
+        <h2>Outras Informações</h2>
+        <p>Esta seção pode conter qualquer outro conteúdo que você deseja adicionar.</p>
+    </div>
+
+    <footer>
+        <p>&copy; 2024 Seu Nome. Todos os direitos reservados.</p>
+    </footer>
+
+</body>
+</html>
