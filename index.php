33c849c32c99d176e7b03306ce6c069eca515503
FAMEC <biancacastro.bc90@gmail.com>
2024-09-05 14:32:22 -0300

    index pronto

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teste Vocacional</title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="shortcut icon" href="assets/img/logo2.jpeg" type="image/x-icon">
 </head>
 <body>
     <header>
@@ -22,12 +23,40 @@
     </div>
 
     <div class="conteudo">
-        <h2>Outras Informações</h2>
-        <p>Esta seção pode conter qualquer outro conteúdo que você deseja adicionar.</p>
+    <div class="container">
+    <img id="mulher" src="assets/img/mulher.png" alt="Imagem de uma mulher">
+    <div class="texto">
+        <h1 id="tv">TESTE VOCACIONAL</h1>
+        <p class="text2">O teste vocacional é uma ferramenta para avaliar e identificar o perfil de cada pessoa e ajudá-la
+            na escolha profissional. Ele tem o objetivo de identificar pontos fortes e habilidades do 
+            estudante para orientar na escolha de qual carreira seguir. <br><br>Também chamado de teste de aptidão, ele contém algumas perguntas para entender 
+            quais cursos mais combinam 
+        com o perfil da pessoa com base nos seus interesses e gostos.</p>
+
+        <p class="text2"></p>
     </div>
+</div>
+<div class="container">
+   
+    <div class="texto">
+    <h2>Como funciona o teste?</h2>
+        <p class="text3">O teste vocacional ajuda você a descobrir a carreira que mais combina com suas habilidades e 
+            interesses. Após responder a um questionário sobre suas preferências e aptidões, o teste analisa suas respostas 
+            para criar um perfil que revela suas principais áreas de interesse e pontos fortes. Com base nesse perfil, são 
+            sugeridas opções de carreiras e cursos que se alinham com suas características. 
+            É uma ferramenta prática para orientar suas escolhas profissionais e ajudar a planejar seu futuro.</p>
+
+    </div>
+     <img id="mulher2" src="assets/img/mulher2.png" alt="Imagem de uma mulher">
+    </div>
+</div>
+
+
+
+            
 
     <footer>
-        <p>&copy; 2024 Seu Nome. Todos os direitos reservados.</p>
+        <p>&copy; 2024 FAMEC. Todos os direitos reservados.</p>
     </footer>
 
 </body>
