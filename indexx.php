cd244ca72ca78bd8447d0b79a89196b0515016c6
FAMEC <manoelleandro.01@hotmail.com>
2024-09-05 14:34:05 -0300

    git

---
+++ indexx.php
@@ -0,0 +1,19 @@
+<?php
+// Iniciar a sessão
+session_start();
+
+// Verificar se o usuário já iniciou o teste
+if (isset($_SESSION['teste_iniciado'])) {
+  header("Location: pergunta.php");
+  exit;
+}
+
+// Iniciar o teste
+$_SESSION['teste_iniciado'] = true;
+$_SESSION['pergunta_atual'] = 0;
+$_SESSION['respostas'] = array();
+
+// Redirecionar para a página de pergunta
+header("Location: pergunta.php");
+exit;
+?>
